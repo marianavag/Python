d71575836983bd50dbbfb4edfea2a33eb6967d89
marianavag <marianavag@hotmail.com>
2026-04-15 12:12:10 +0100

    Delete Module07 directory

--- Module07/ex0/__init__.py
+++
@@ -1,3 +0,0 @@
-from .creature_factory import CreatureFactory, FlameFactory, AquaFactory
-
-__all__ = ["FlameFactory", "AquaFactory", "CreatureFactory"]
--- Module07/ex2/__init__.py
+++
@@ -1,3 +0,0 @@
-from .battle_strategy import BattleStrategy, NormalStrategy, AgressiveStrategy, InvalidStrategyError
-
-__all__ = ["BattleStrategy", "NormalStrategy", "AgressiveStrategy", "InvalidStrategyError"]
